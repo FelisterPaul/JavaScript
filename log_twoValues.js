2b1643bc455a3b465baf9997f47aa807c484fdf2
stecy1 <felisterpaul4@gmail.com>
2023-06-19 07:53:17 +0300

    remove html lines of code

--- log_twoValues.js
+++ log_twoValues.js
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <Head>
-        <title>   
-            <script>
+
         let thisVar = 'first value';
         let thatVar = 'second value';
         console.log("thisVar:", thisVar, "and thatVar:", thatVar);
-            </script>
-         <title/>
-        </Head>
-</html>
+       
