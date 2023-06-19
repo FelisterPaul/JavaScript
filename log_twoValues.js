d28185135daedb982b50df3448b3e1c866ea164d
stecy1 <felisterpaul4@gmail.com>
2023-06-19 07:50:33 +0300

    log two values in one statement

---
+++ log_twoValues.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <Head>
+        <title>   
+            <script>
+        let thisVar = 'first value';
+        let thatVar = 'second value';
+        console.log("thisVar:", thisVar, "and thatVar:", thatVar);
+            </script>
+         <title/>
+        </Head>
+</html>
