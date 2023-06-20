857b440ab80309dd5551811d052ecfaee35bc6d7
stecy1 <felisterpaul4@gmail.com>
2023-06-20 07:59:39 +0300

    DOM API

---
+++ dom_api.js
@@ -0,0 +1,3 @@
+// This will select the element that with the id paragraph and set its text content to "Hello, World":
+// <p id="paragraph">Hello, World</p>
+document.getElementById("paragraph").textContent = "Hello, World";
