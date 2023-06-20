f5fa292353fb90b64f8610705a2bd246421a57fb
stecy1 <felisterpaul4@gmail.com>
2023-06-20 07:47:03 +0300

    logging objects

---
+++ logging_objects.js
@@ -0,0 +1,10 @@
+console.log({
+    'Email':'',
+    'Groups':{},
+    'Id': 33,
+    'IsHiddenInUI':false
+    'IsSiteAdmin': false
+    'LoginName': '1:0#w|virtualdomain\\users2;'
+    'PrincipalType':1,
+    'Title': 'user2;'
+})
